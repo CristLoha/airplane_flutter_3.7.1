7c860a23ae9e8c89bb5cef0550c229acce654b36
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-02-13 16:29:47 +0800

    update

--- lib/ui/pages/splash/splash_page.dart
+++ lib/ui/pages/splash/splash_page.dart
@@ -1,11 +1,27 @@
+import 'dart:async';
+
 import 'package:airplane/shared/box_extension.dart';
+import 'package:airplane/ui/pages/get_started/get_started_page.dart';
 import 'package:airplane/ui/pages/splash/components/splash_logo.dart';
 import 'package:flutter/material.dart';
 import '../../../shared/theme.dart';
 import 'components/splash_tittle.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushNamed(context, '/get-started');
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
