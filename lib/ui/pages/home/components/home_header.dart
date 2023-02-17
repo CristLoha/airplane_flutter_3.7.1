f17e50714b0232a3c37a4df92bfa99f02e3cca71
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-02-17 11:09:54 +0800

    update

--- lib/ui/pages/home/components/home_header.dart
+++ lib/ui/pages/home/components/home_header.dart
@@ -8,44 +8,49 @@ class HomeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Howdy,\nKezia Anne',
-                style: blackTextStyle.copyWith(
-                  fontSize: 24,
-                  fontWeight: semiBold,
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: defaultMargin,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Howdy,\nKezia Anne',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: semiBold,
+                  ),
+                  overflow: TextOverflow.ellipsis,
                 ),
-                overflow: TextOverflow.ellipsis,
-              ),
-              6.heightBox,
-              Text(
-                'Where to fly today?',
-                style: greyTextStyle.copyWith(
-                  fontSize: 16,
-                  fontWeight: light,
+                6.heightBox,
+                Text(
+                  'Where to fly today?',
+                  style: greyTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: light,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-              image: AssetImage(
-                ImgString.imageProfile,
-              ),
+              ],
             ),
           ),
-        )
-      ],
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  ImgString.imageProfile,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:airplane/shared/box_extension.dart';
-import 'package:airplane/shared/theme.dart';
 import 'package:airplane/ui/pages/home/components/home_header.dart';
 import 'package:airplane/ui/pages/home/components/home_popular_destination.dart';
 import 'package:flutter/material.dart';
